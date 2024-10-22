3ec8f534102f26b30a80ce96eccf40dd1734482b
windowslee <slee12463@gmail.com>
2024-10-22 13:48:26 -0400

    tweek

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -24,7 +24,7 @@ const HeroSection = () => {
       </div>
       <div className="absolute inset-0 -z-10 bg-gradient-to-b from-transparent from-70% to-black" />
       <div className="relative z-20 flex h-screen flex-col justify-end pb-20">
-        <p className="text-white text-bold">COMING SOON</p>
+        <p className="text-white text-bold text-sm">COMING SOON</p>
         <motion.img
           initial={{ opacity: 0, y: 80 }}
           animate={{ opacity: 1, y: 80 }}
