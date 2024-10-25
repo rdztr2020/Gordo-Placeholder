7dfaf20f042cb94bfd6a5ffd586dfca67d997e71
windowslee <slee12463@gmail.com>
2024-10-24 21:55:19 -0400

    tweek

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -9,7 +9,11 @@ const HeroSection = () => {
       className="relative flex h-screen items-center justify-center"
       id="top"
     >
+      
       <div className="absolute inset-0 -z-29 h-full w-full overflow-hidden">
+      <p className="p-4 text-xl tracking-tighter text-red-700 font-extrabold">
+          Coming Soon
+        </p>
         <video
           src={video}
           className="h-full w-full object-cover"
@@ -19,17 +23,20 @@ const HeroSection = () => {
           playsInline
           poster={hero}
         ></video>
+        
       </div>
       <div className="absolute inset-0 -z-10 bg-gradient-to-b from-transparent from-70% to-black" />
-      <div className="relative z-20 flex h-screen flex-col justify-end pb-30">
+      <div className="relative z-20 flex flex-col justify-end pb-30">
+      
         <motion.img
           initial={{ opacity: 0, y: 0}}
-          animate={{ opacity: 1, y: -150}}
+          animate={{ opacity: 1, y: -100}}
           transition={{ duration: 3 }}
           src={logo}
           alt="Gordo Mikes"
           className="w-80 p-4"
         />
+        
       </div>
     </section>
   );
