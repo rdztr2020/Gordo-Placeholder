36c8173c0e1d67b31c261cd9e0e0ff96badc8aad
windowslee <slee12463@gmail.com>
2024-10-27 18:26:20 -0400

    tweek

--- src/components/Comingsoon2.jsx
+++ src/components/Comingsoon2.jsx
@@ -1,11 +1,9 @@
-import logo from "../assets/logotransparent.png";
-import DesktopImage from "../assets/flamespost.jpg"
 
 const Comingsoon2 = () => {
   return (
     <section >
       <div >
-      <p className="p-4 text-xl tracking-tighter text-red-700 font-extrabold">
+      <p className="p-2 text-xl tracking-tighter text-red-700 font-extrabold">
           Coming Soon
         </p>
       </div>
